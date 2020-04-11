3845ad0ea561ccbbc8f2b6d1271386084af027f3
Visa <visalakshi.cse@gmail.com>
2020-04-11 18:31:34 -0500

    delete functionality included

--- wine_reactapp/src/App.js
+++ wine_reactapp/src/App.js
@@ -15,9 +15,10 @@ constructor(props){
   this.state={
 
     winedata:[],
-    picture:[]
+    picture:[],
+    id:[]
   }
-  
+  this.deletewine=this.deletewine.bind(this);
 }
 
 async getData(){
@@ -32,11 +33,28 @@ this.getPictures();
   }
 }
 
+deletewine=event=>{
+console.log("delete entered",event.target.name)
+  
+    event.preventDefault();
+
+    axios.delete(`https://myapi-profstream.herokuapp.com/api/07dab0/wines/${event.target.name}`)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+      this.getData();
+}
+
 getPictures(){
   let pic=[];
+  let ids=[];
 for(let i=0;i<=this.state.winedata.length;i++){
 pic.push(this.state.winedata[i].picture);
-this.setState({picture:pic})
+ids.push(this.state.winedata[i].id);
+this.setState({picture:pic});
+this.setState({id:ids});
+
 
 }
 
@@ -49,41 +67,72 @@ this.getData();
 }  
 render(){
   return (
+  
     
       <Router>
+            <h1 > WineStore </h1>
         <div className="App">
+        
+     <div>
+     
         <Link  to="/wine1">
 <img src={this.state.picture[0]}/> 
+
 </Link >
+<input type="button" name={this.state.id[0]} className="btn" value="X" onClick={this.deletewine}></input>
 <Route exact path="/wine1"><Wine1 winedetail={this.state.winedata} /></Route> 
 
+</div>
 
- 
- <Link to="/wine1">
+
+
+
+<div>
+ <Link to="/wine2">
 <img src={this.state.picture[1]}/>   
 </Link >
-
+<input type="button" name={this.state.id[1]} className="btn" value="X" onClick={this.deletewine}></input>
+<Route exact path="/wine2"><Wine2 winedetail={this.state.winedata} /></Route> 
+</div>
+<div>
 <Link to="/wine1">
 <img src={this.state.picture[2]}/>   
 </Link>
+<input type="button" name={this.state.id[2]} className="btn" value="X" onClick={this.deletewine}></input>
+</div>
+<div>
 <Link to="/wine1">
 
 <img src={this.state.picture[3]}/>  
 </Link>
+<input type="button" name={this.state.id[3]} className="btn" value="X" onClick={this.deletewine}></input>
+</div>
+<div>
 <Link to="/wine1">
 <img src={this.state.picture[4]}/>    
 </Link>
+<input type="button" name={this.state.id[4]} className="btn" value="X" onClick={this.deletewine}></input>
+</div>
+<div>
 <Link to="/wine1">
 <img src={this.state.picture[5]}/> 
 </Link>
+<input type="button" name={this.state.id[5]} className="btn" value="X" onClick={this.deletewine}></input>
+</div>
+<div>
 <Link to="/wine1">
 <img src={this.state.picture[6]}/> 
 </Link>
+<input type="button" name={this.state.id[6]} className="btn" value="X" onClick={this.deletewine}></input>
+</div>
+<div>
 <Link to="/wine1">   
 <img src={this.state.picture[7]}/> 
-</Link>   
+</Link>  
+<input type="button" name={this.state.id[7]} className="btn" value="X" onClick={this.deletewine}></input> 
+</div>
 
-<Route exact path="/wine1"><Wine1 winedetail={this.state.winedata} /></Route>
+
 
 
 </div>
@@ -92,5 +141,23 @@ render(){
   );
 }
 }
+
+ function Wine2(props)
+{
+    var wine=[];
+    wine=props.winedetail[1];
+    if(wine===undefined)
+    return;
+    console.log(wine);
+    return(
+        <div key={wine["id"]} className="wine" tabIndex="1" >
+          <h4>{wine["name"]} ({wine["year"]})</h4>
+          <h4>{wine["grapes"]}</h4>
+          <h4>{wine["country"], wine["region"]}</h4>
+          <p>{wine["description"]}</p>
+        </div>  
+    );
+}
+
 
 export default App;
--- wine_reactapp/src/Components/Wine1.js
+++ wine_reactapp/src/Components/Wine1.js
@@ -5,15 +5,11 @@ export default function Wine1(props)
     wine=props.winedetail[0];
     console.log(wine);
     return(
-<div key={wine["id"]} className="wine" tabIndex="1" >
-        
+        <div key={wine["id"]} className="wine" tabIndex="1" >
           <h4>{wine["name"]} ({wine["year"]})</h4>
           <h4>{wine["grapes"]}</h4>
           <h4>{wine["country"], wine["region"]}</h4>
           <p>{wine["description"]}</p>
-          </div>  
-    
-  
+        </div>  
     );
-    
 }
