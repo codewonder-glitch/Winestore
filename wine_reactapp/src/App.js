6f11b454e691861d6a7fa475feb42ec3963e4042
Visa <visalakshi.cse@gmail.com>
2020-04-12 23:12:15 -0500

    Removed getpicture function

--- wine_reactapp/src/App.js
+++ wine_reactapp/src/App.js
@@ -36,7 +36,6 @@ constructor(props){
 
 handleChange(event) {
   this.setState({[event.target.name]: event.target.value})
-  // this.setState({Breed: event.target.Breed});
   console.log(event.target.name,event.target.value)
 }
 async getData(){
@@ -44,23 +43,22 @@ async getData(){
 const response=await axios.get("https://myapi-profstream.herokuapp.com/api/07dab0/wines");
 this.setState({winedata:response.data})
 console.log(this.state.winedata)
-
-
-let i=0;
-let elements=response.data.map(wine=>{
+let i=-1;
+let elements=this.state.winedata.map(wine=>{
+  {i++}
   return(
   <div>
-  <Link  to={`wine`+i}>
-<img src={wine.picture}/> 
 
+<Link  to={`wine`+i}>
+<img src={wine.picture}/> 
 </Link >
+ 
  <input type="button" name={wine.id} className="btn" value="X" onClick={this.deletewine}></input>
  
-<Route exact path="wine0" ><Wine1 winedata={wine[i]} /> </Route>
-
+<Route exact path={`wine`+i} ><Wine1 winedata={wine[i]} /> </Route>
 
 </div>)
-{i++}
+
 })
     this.setState({winerender:elements})
   console.log(this.state.winerender)
@@ -79,18 +77,19 @@ console.log("delete entered",event.target.name)
 
     axios.delete(`https://myapi-profstream.herokuapp.com/api/07dab0/wines/${event.target.name}`)
       .then(res => {
-        console.log(res);
-        console.log(res.data);
-      })
-      this.getData();
+        console.log("deleted event is",res);
+        console.log("deleted event is",res.data);
+        this.getData();
+    })
+     
 }
 
 handleCreate=event=>{
   console.log("create entered",event.target.name)
-    console.log(this.state)
-      event.preventDefault();
+  console.log(this.state)
+  event.preventDefault();
   
-      axios.post('https://myapi-profstream.herokuapp.com/api/07dab0/wines', {
+    axios.post('https://myapi-profstream.herokuapp.com/api/07dab0/wines', {
         name: this.state.name1,
         year: this.state.year,
         grapes:this.state.grapes,
@@ -99,25 +98,9 @@ handleCreate=event=>{
         description:this.state.description,
         picture:this.state.cpic,
         price:this.state.price
-      })
+    })
       this.getData();
   }
-
-  
-
-getPictures(){
-  let pic=[];
-  let ids=[];
-for(let i=0;i<=this.state.winedata.length;i++){
-pic.push(this.state.winedata[i].picture);
-ids.push(this.state.winedata[i].id);
-this.setState({picture:pic});
-this.setState({id:ids});
-
-
-}
-
-}
 
 componentDidMount(){
 this.getData();
@@ -125,148 +108,87 @@ this.getData();
 
 }  
 render(){  
-  let elem1=()=>{
-    console.log("Are u coming here out")
-  if(this.state.picture[0]!==undefined)
-  {
-    console.log("Are u coming here insi")
-      return(
-    <div>
     
-       <Link  to="/wine1">
-<img src={this.state.picture[0]}/> 
-
-</Link >
-<input type="button" name={this.state.id[0]} className="btn" value="X" onClick={this.deletewine}></input>
-<Route exact path="/wine1"><Wine1 winedetail={this.state.winedata} /></Route> 
-
-</div>);}}
   return (
     
   
-    
-      <Router>
-             <h1>WineStore</h1>
+    <Router>
+   
+        <h1>WineStore</h1>
         <div className="App">
     
-{this.state.winerender}
+        {this.state.winerender}
 
-<form id="form1" onSubmit={this.handleChange}>
+      <form id="form1" onSubmit={this.handleChange}>
         
         <label>  
         Name: 
-          <br />
-          <input name="name1" type="text"  value={this.state.name1} onChange={this.handleChange}/>
-          </label>
-          <br />
-          <label>
+        <br />
+        <input name="name1" type="text"  value={this.state.name1} onChange={this.handleChange}/>
+        </label>
+        <br />
+
+        <label>
         Year:
-            <br />
-          <input name="year" type="text"  value={this.state.year} onChange={this.handleChange}/>
-          </label>
-          <br />
-          <label> 
-         Grapes:
-          <br />
-          <input name="grapes" type="text"  value={this.state.grapes} onChange={this.handleChange}/>
-          </label>
-          <br />
-          <label> 
-          Country:
-          <br />
-          <input name="country" type="text"  value={this.state.country} onChange={this.handleChange}/>
-          </label>
-          <br />
+        <br />
+        <input name="year" type="text"  value={this.state.year} onChange={this.handleChange}/>
+        </label>
+        <br />
+
+        <label> 
+        Grapes:
+        <br />
+        <input name="grapes" type="text"  value={this.state.grapes} onChange={this.handleChange}/>
+        </label>
+        <br />
+
+        <label> 
+        Country:
+        <br />
+        <input name="country" type="text"  value={this.state.country} onChange={this.handleChange}/>
+        </label>
+        <br />
           
-          <label> 
-          Region:
-          <br />
-          <input name="region" type="text"  value={this.state.region} onChange={this.handleChange}/>
-          </label>
-          <br />
-          <label> 
-          Description:
-          <br />
-          <input name="description" type="text"  value={this.state.description} onChange={this.handleChange}/>
-          </label>
-          <br />
+        <label> 
+        Region:
+        <br />
+        <input name="region" type="text"  value={this.state.region} onChange={this.handleChange}/>
+        </label>
+        <br />
+
+        <label> 
+        Description:
+        <br />
+        <input name="description" type="text"  value={this.state.description} onChange={this.handleChange}/>
+        </label>
+        <br />
         
-          <label> 
-          Picture:
-          <br />
-          <input name="cpic" type="text"  value={this.state.cpic} onChange={this.handleChange}/>
-          </label>
+        <label> 
+        Picture:
+        <br />
+        <input name="cpic" type="text"  value={this.state.cpic} onChange={this.handleChange}/>
+        </label>
+        <br />
 
-          <br />
-          <label>
-          Price:
-          <br />
-          <input name="price" type="text"  value={this.state.price} onChange={this.handleChange}/>
-          </label>
+        <label>
+        Price:
+        <br />
+        <input name="price" type="text"  value={this.state.price} onChange={this.handleChange}/>
+        </label>
+        <br />
 
-         
-          <br />
-          <label>
+        <label>
+        <br />
+        <input id="create" name="create" type="button"  value="Create" onClick={this.handleCreate}/>
+        </label>
           
-          <br />
-          <input id="create" name="create" type="button"  value="Create" onClick={this.handleCreate}/>
-          </label>
-          
-          </form>
+        </form>
 
-{/* <div>
- <Link to="/wine2">
-<img src={this.state.picture[1]}/>   
-</Link >
-<input type="button" name={this.state.id[1]} className="btn" value="X" onClick={this.deletewine}></input>
-<Route exact path="/wine2"><Wine2 winedetail={this.state.winedata} /></Route> 
-</div>
-<div>
-<Link to="/wine1">
-<img src={this.state.picture[2]}/>   
-</Link>
-<input type="button" name={this.state.id[2]} className="btn" value="X" onClick={this.deletewine}></input>
-</div>
-<div>
-<Link to="/wine1">
+    </div>
+    </Router>
 
-<img src={this.state.picture[3]}/>  
-</Link>
-<input type="button" name={this.state.id[3]} className="btn" value="X" onClick={this.deletewine}></input>
-</div>
-<div>
-<Link to="/wine1">
-<img src={this.state.picture[4]}/>    
-</Link>
-<input type="button" name={this.state.id[4]} className="btn" value="X" onClick={this.deletewine}></input>
-</div>
-<div>
-<Link to="/wine1">
-<img src={this.state.picture[5]}/> 
-</Link>
-<input type="button" name={this.state.id[5]} className="btn" value="X" onClick={this.deletewine}></input>
-</div> */}
-{/* <div>
-<Link to="/wine1">
-<img src={this.state.picture[6]}/> 
-</Link>
-<input type="button" name={this.state.id[6]} className="btn" value="X" onClick={this.deletewine}></input>
-</div>
-<div>
-<Link to="/wine1">   
-<img src={this.state.picture[7]}/> 
-</Link>  
-<input type="button" name={this.state.id[7]} className="btn" value="X" onClick={this.deletewine}></input> 
-</div> */}
-
-
-
-
-</div>
-</Router>   
-   
-  );
-}
+    );
+    }
 }
 
  function Wine1(props)
